a3a59f801f910e90e7ddfad70bd4145b6d056d9f
wafuwafu13 <wafuwafu1313@gmail.com>
2021-11-10 20:54:02 +0900

    a

--- in.js
+++ in.js
@@ -1,1 +1,8 @@
-const fakeComponent = fake(<Fake />);
+describe("node-assert-2-jest-expect", () => {
+    it("assert", () => {
+      const one = 1;
+      fake(one);
+      const fake = fake();
+      const fakeComponent = fake(<Fake />);
+    });
+});
--- out.js
+++ out.js
@@ -1,1 +1,8 @@
-const fakeComponent = fake( /*#__PURE__*/React.createElement(Fake, null));
+describe("node-assert-2-jest-expect", () => {
+  it("assert", () => {
+    const one = 1;
+    fake(one);
+    const fake = fake();
+    const fakeComponent = fake( /*#__PURE__*/React.createElement(Fake, null));
+  });
+});
